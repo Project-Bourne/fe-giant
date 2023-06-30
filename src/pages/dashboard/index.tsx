b490c64693bef19535027b405748a153236249dc
henry  okafor <hoc@henrys-MacBook-Pro.local>
2023-06-30 07:45:39 +0100

    bug-fix: merge conflict

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Left from './components/LeftCompt'
-import Right from './components/RightCompt'
+import React from "react";
+import Left from "./components/LeftCompt";
+import Right from "./components/RightCompt";
+import Group1 from "./components/Group1";
 
 const index = () => {
   return (
     <div>
-      <h1 className='text-black text-2xl pl-10 font-bold'>Welcome Oluanrawaju</h1>
+      <h1 className="text-black text-2xl pl-10 font-bold">
+        Welcome Oluanrawaju
+      </h1>
 
-      <div className='flex items-center justify-between w-full gap-[20px] mt-5'>
-      <Left />
-      <Right/>
+      {/* the yellow navigation at the top of the dashboard page */}
+      <div className="flex items-center justify-between w-full gap-[20px] mt-5">
+        <Left />
+        <Right />
       </div>
-      
-    </div>  )
-}
+      <div className="mt-5">
+        <Group1 />
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;
