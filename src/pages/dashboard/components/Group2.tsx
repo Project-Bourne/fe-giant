2a2bdbe88e516b4c3060c83688214618744cf597
henry  okafor <hoc@henrys-MacBook-Pro.local>
2023-06-30 07:51:45 +0100

    bug-fix: merge conflict

--- src/pages/dashboard/components/Group2.tsx
+++ src/pages/dashboard/components/Group2.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-function Group1() {
+function Group2() {
   return (
     <div className="flex flex-row gap-5 ml-5 mr-5">
       <div className="border border-sirp-dashbordb1 rounded-[1.5rem] bg-sirp-secondary2 basis-1/3 h-[12rem] mt-5  pt-3">
@@ -9,7 +9,7 @@ function Group1() {
           <div className="flex flex-row items-center gap-3 pt-5">
             <div>
               <Image
-                src={require("../../../assets/icons/Frame 5.svg")}
+                src={require("../../../assets/icons/Frame 8.svg")}
                 alt="documents"
                 className="pl-10 cursor-pointer"
                 width={130}
@@ -18,46 +18,23 @@ function Group1() {
             <div>
               <p className="font-bold">4000</p>
               <span className="font-light text-sirp-grey">
-                Total content crawled
+                Total documents analyzed
               </span>
             </div>
           </div>
         </div>
         <div className="ml-6 pt-8 ">
           <button className=" border border-sirp-dashbordb w-[20rem] pb-2 pt-2 rounded-[1rem] hover:bg-sirp-dashbordb hover:text-white text-sirp-dashbordb font-bold">
-            Open SIRP
+            Open Analizer
           </button>
         </div>
       </div>
-      <div className="border border-sirp-dashbordb1 rounded-[1.5rem] bg-sirp-secondary2 basis-1/3 h-[12rem] mt-5  pt-3">
+            <div className="border border-sirp-dashbordb1 rounded-[1.5rem] bg-sirp-secondary2 basis-1/3 h-[12rem] mt-5  pt-3">
         <div>
           <div className="flex flex-row items-center gap-3 pt-5">
             <div>
               <Image
-                src={require("../../../assets/icons/Frame 6.svg")}
-                alt="documents"
-                className="pl-10 cursor-pointer"
-                width={115}
-              />
-            </div>
-            <div>
-              <p className="font-bold">4000</p>
-              <span className="font-light text-sirp-grey">Total users</span>
-            </div>
-          </div>
-        </div>
-        <div className="ml-6 pt-8 ">
-          <button className=" border border-sirp-dashbordb3 hover: w-[20rem] pb-2 pt-2 rounded-[1rem] text-sirp-dashbordb3a hover:bg-sirp-dashbordb3a hover:text-white font-bold ">
-            Invite collaborators
-          </button>
-        </div>
-      </div>
-      <div className="border border-sirp-dashbordb1 rounded-[1.5rem] bg-sirp-secondary2 basis-1/3 h-[12rem] mt-5  pt-3">
-        <div>
-          <div className="flex flex-row items-center gap-3 pt-5">
-            <div>
-              <Image
-                src={require("../../../assets/icons/Frame 7.svg")}
+                src={require("../../../assets/icons/Frame 9.svg")}
                 alt="documents"
                 className="pl-10 cursor-pointer"
                 width={130}
@@ -66,14 +43,39 @@ function Group1() {
             <div>
               <p className="font-bold">4000</p>
               <span className="font-light text-sirp-grey">
-                Total exports to collab
+                Total documents analyzed
               </span>
             </div>
           </div>
         </div>
         <div className="ml-6 pt-8 ">
           <button className=" border border-sirp-dashbordb w-[20rem] pb-2 pt-2 rounded-[1rem] hover:bg-sirp-dashbordb hover:text-white text-sirp-dashbordb font-bold">
-            Open collab workspace
+            Open Summarizer
+          </button>
+        </div>
+      </div>
+      <div className="border border-sirp-dashbordb1 rounded-[1.5rem] bg-sirp-secondary2 basis-1/3 h-[12rem] mt-5  pt-3">
+        <div>
+          <div className="flex flex-row items-center gap-3 pt-5">
+            <div>
+              <Image
+                src={require("../../../assets/icons/Frame 10.svg")}
+                alt="documents"
+                className="pl-10 cursor-pointer"
+                width={130}
+              />
+            </div>
+            <div>
+              <p className="font-bold">4000</p>
+              <span className="font-light text-sirp-grey">
+                Total documents Translated
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="ml-6 pt-8 ">
+          <button className=" border border-sirp-dashbordb w-[20rem] pb-2 pt-2 rounded-[1rem] hover:bg-sirp-dashbordb hover:text-white text-sirp-dashbordb font-bold">
+            Open Translator
           </button>
         </div>
       </div>
@@ -81,4 +83,4 @@ function Group1() {
   );
 }
 
-export default Group1;
+export default Group2;
