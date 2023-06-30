a77f59cf8eeea937f24eb8cf9cf7cd9445a632a3
Legacy <legacy.rexio@gmail.com>
2023-06-30 10:13:13 +0100

    Settings Tab Stash1

--- src/pages/settings/components/TabComp/index.tsx
+++ src/pages/settings/components/TabComp/index.tsx
@@ -26,7 +26,8 @@ const TabComp = ({item, index}: TabCompType) => {
       : 'px-8 pt-3 flex flex-row items-center border-b pb-3 mr-15 mb-[-2px] cursor-pointer'
     }>
       <Image
-        src={item.route.includes(route) ? require(`../../../../assets/icons/on.${item.icon}`) : require(`../../../../assets/icons/${item.icon}`)}
+        src={require(`../../../../assets/icons/${item.icon}`)}
+        // item.route.includes(route) ? require(`../../../../assets/icons/on.${item.icon}`) : 
         alt="settings tab"
         width={18}
         height={18}
