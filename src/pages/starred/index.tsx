a055f57a0ea562b89c2c6c0436294b3835909c8d
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-07-05 13:00:57 +0100

    solve

--- src/pages/starred/index.tsx
+++ src/pages/starred/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import HeadIcon from "./components/HeadIcon";
+import HeadIconTwo from "./components/HeadIconTwo";
+import Content from "./components/Content";
+import dummy from "../../../dummy.json";
 
 function Starred() {
+  const [activeOption, setActiveOption] = useState("All");
+  const [dummyData, setDummyData] = useState(dummy);
+
+  const handleOptionChange = (option) => {
+    setActiveOption(option);
+  };
+
+  const filteredData = activeOption === "All"
+    ? dummyData
+    : dummyData.filter((item) => item.isMarked === false);
+
   return (
-    <div>Starred</div>
-  )
+    <div className="bg-sirp-contentbg h-[100%] pt-5 mx-10 rounded-[1rem]">
+      <HeadIcon />
+      <HeadIconTwo activeOption={activeOption} onOptionChange={handleOptionChange} />
+      <Content data={filteredData} />
+    </div>
+  );
 }
 
-export default Starred
+export default Starred;
