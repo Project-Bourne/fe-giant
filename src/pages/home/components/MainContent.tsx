7c7ad819568c20e1f89d440f0abe78ddd6a30d9e
okafohenry <okafohenrie@gmail.com>
2023-10-23 14:23:42 +0100

    bug fix

--- src/pages/home/components/MainContent.tsx
+++ src/pages/home/components/MainContent.tsx
@@ -6,14 +6,24 @@ function MainContent({
   content,
   // image,
 }) {
+  if (!content) return;
+
+  const responseArray = content.split("\n");
+
   return (
     <div>
       <header>
-        <h2 className="text-3xl capitalize mb-5">{title}</h2>
+        <h2 className="text-2xl capitalize">{title}</h2>
       </header>
       <main>
-        <p className="first-letter:capitalize text-justify leading-8 text-[1rem] mb-10">
-          {content}
+        <p className="first-letter:capitalize text-justify leading-6 text-[1rem] mb-10">
+          {responseArray.map((line, index) => (
+            <React.Fragment key={index}>
+              {line}
+              {index !== responseArray.length - 1 && <br />}{" "}
+              {/* Add <br /> except for the last line */}
+            </React.Fragment>
+          ))}
         </p>
       </main>
     </div>
